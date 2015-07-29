6a86f7a489a02a5f1fcecb048990d007dfae56a6
Kevin Nickels <knickels@trinity.edu>
2015-07-29 18:27:50 -0500

    splitting pyc/pycpp

---
+++ code/Ch8.3/bigbob8.py
@@ -0,0 +1,29 @@
+# Original author: K. Nickels 7/24/13
+import sys, os, math
+# This should be wherever "playercpp.py" is
+sys.path.append('/usr/local/lib/python2.7/site-packages/')
+from playercpp import *
+# Define our ppstr and ppdstr, so that we may use it later
+ppstr= '';
+ppdstr= '';
+
+# read from the proxies
+robot = PlayerClient("localhost");
+pp = Position2dProxy(robot,0);
+# for carlike robots
+# p2dProxy.SetSpeed(0.1,0.1); # XSpeed (m/s), YawSpeed (rad/s)
+pp.SetCarlike(0.2,math.radians(3.0)); # XSpeed, Yaw (rad)
+# for holonomic robots
+# pp.SetSpeed(0.1,0.1,0.1); # XSpeed (m/s), YawSpeed (rad/s)
+
+for i in range(10):
+	robot.Read()
+	print 'Iter ',i
+	print 'XSpeed = %.3f m/s, ' % pp.GetXSpeed(), 
+	print 'YSpeed = %.3f m/s, ' % pp.GetYSpeed(), 
+	print 'YawSpeed = %.3f rad/s ' % pp.GetYawSpeed()
+	print 'XPos = %.3f m, ' % pp.GetXPos(),
+	print 'YPos = %.3f m, ' % pp.GetYPos(),
+	print 'Yaw = %.3f rad' % pp.GetYaw() 
+
+# Destructors called automatically on exit.
